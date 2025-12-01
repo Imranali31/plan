bd5517a6eeb3f818e4c3a112ddf6d08926219b11
Imran <imran@bdjobs.local>
2025-12-01 19:00:14 +0600

    First commit

---
+++ app/src/main/java/com/example/planandpricing/ProPlan.kt
@@ -0,0 +1,57 @@
+package com.example.planandpricing
+
+
+import com.google.gson.annotations.SerializedName
+import androidx.annotation.Keep
+
+@Keep
+data class ProPlan(
+    @SerializedName("common")
+    val common: Any,
+    @SerializedName("data")
+    val `data`: List<Data>,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("statusCode")
+    val statusCode: Int
+) {
+    @Keep
+    data class Data(
+        @SerializedName("duration")
+        val duration: String,
+        @SerializedName("durationCount")
+        val durationCount: Int,
+        @SerializedName("packages")
+        val packages: List<Package>
+    ) {
+        @Keep
+        data class Package(
+            @SerializedName("discount")
+            val discount: Double,
+            @SerializedName("discountPercentage")
+            val discountPercentage: String,
+            @SerializedName("feature")
+            val feature: List<Feature>,
+            @SerializedName("hasPackage")
+            val hasPackage: Int,
+            @SerializedName("packageId")
+            val packageId: Int,
+            @SerializedName("packageName")
+            val packageName: String,
+            @SerializedName("price")
+            val price: Double,
+            @SerializedName("serviceId")
+            val serviceId: Int
+        ) {
+            @Keep
+            data class Feature(
+                @SerializedName("featureId")
+                val featureId: Int,
+                @SerializedName("featureName")
+                val featureName: String,
+                @SerializedName("quantity")
+                val quantity: Int
+            )
+        }
+    }
+}
